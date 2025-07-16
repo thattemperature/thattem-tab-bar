a57ed3f2d17c79a98356cd7fe9e664d8ce61245f
thattemperature <2719023332@qq.com>
2025-07-16 17:08:46 +0800

    Change file header.

--- thattem-tab-bar-faces.el
+++ thattem-tab-bar-faces.el
@@ -1,6 +1,23 @@
-;;; Tab-bar-faces --- define faces used in tab bar. ;; -*- lexical-binding: t; -*-
+;;; Tab-bar-faces --- define faces used in tab bar  -*- lexical-binding: t; -*-
+
+;; Author: That Temperature <2719023332@qq.com>
+;; URL: https://github.com/thattemperature/thattem-tab-bar
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
+
 ;; Tab-bar-faces defines the text face used in Thattem-tab-bar.
 
 ;;; Code:
--- thattem-tab-bar.el
+++ thattem-tab-bar.el
@@ -1,6 +1,24 @@
-;;; Thattem-tab-bar --- a simple tab bar. ;; -*- lexical-binding: t; -*-
+;;; Thattem-tab-bar --- a simple tab bar  -*- lexical-binding: t; -*-
+
+;; Author: That Temperature <2719023332@qq.com>
+;; Package-Requires: ((emacs "30.1") nerd-icons)
+;; URL: https://github.com/thattemperature/thattem-tab-bar
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
+
 ;; Thattem-tab-bar is a package that change the appearance of the
 ;; default tab bar.  It DO NOT change the items used in tab bar,
 ;; like "tab-bar-format-tabs".  Instead, it defines replacement
@@ -10,9 +28,6 @@
 ;; as default.  You can also customize this variable.
 
 ;;; Code:
-
-;; Dependencies
-(require 'nerd-icons)
 
 (defgroup thattem-tab-bar nil
   "Modified tab bar."
